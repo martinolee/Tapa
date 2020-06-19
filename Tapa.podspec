3d82ba6ac261fecb19fa8f8184dc6e9ea3a7d5f3
panda <gusauddml@gmail.com>
2020-06-20 07:25:10 +0900

    relese: 0.1.1

--- Tapa.podspec
+++ Tapa.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'Tapa'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'Make View with Tapa.'
   s.description      = 'Convenient extensions associated with UI'
   s.homepage         = 'https://github.com/wanderingfairy/Tapa'
--- Tapa/Classes/ReplaceMe.swift
+++ Tapa/Classes/ReplaceMe.swift
@@ -1,12 +1,33 @@
 import UIKit
 
 extension UIView {
+  //addSubview multiple UIView at a time
   func addSubViews(views: [UIView]) {
     views.forEach({ addSubview($0) })
+  }
+  
+  //Make the corners of the UIView round.
+  func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+       let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+       let mask = CAShapeLayer()
+       mask.path = path.cgPath
+       self.layer.mask = mask
+  }
+  
+  //Find Parent ViewController
+  var viewController: UIViewController? {
+    if let vc = self.next as? UIViewController {
+      return vc
+    } else if let superview = self.superview {
+      return superview.viewController
+    } else {
+      return nil
+    }
   }
 }
 
 extension UITextField {
+  //Add left and right insets of UITextField
   func addLeftPadding(_ inset: Int = 10) {
     let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: CGFloat(inset), height: self.frame.height))
     self.leftView = paddingView
