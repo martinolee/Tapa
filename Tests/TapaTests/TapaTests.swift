d1585c3ee9ecf5b82edf1dc3a1dfbd587d7f1e53
panda <gusauddml@gmail.com>
2020-06-20 12:43:52 +0900

    Tapa폴더를 Sources 폴더 아래로 옮기고 test코드 전체 주석처리

--- Tests/TapaTests/TapaTests.swift
+++ Tests/TapaTests/TapaTests.swift
@@ -1,15 +1,15 @@
-import XCTest
-@testable import Tapa
+//import XCTest
+//@testable import Tapa
 
-final class TapaTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Tapa().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
-}
+//final class TapaTests: XCTestCase {
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+////        XCTAssertEqual(Tapa().text, "Hello, World!")
+//    }
+//
+//    static var allTests = [
+////        ("testExample", testExample),
+//    ]
+//}
--- Tests/TapaTests/XCTestManifests.swift
+++ Tests/TapaTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(TapaTests.allTests),
-    ]
-}
-#endif
+//import XCTest
+//
+//#if !canImport(ObjectiveC)
+//public func allTests() -> [XCTestCaseEntry] {
+//    return [
+//        testCase(TapaTests.allTests),
+//    ]
+//}
+//#endif
